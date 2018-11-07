0a8ead1f539f5515a7076fd79be04a721eb50cef
Ana Vargas <avargas000@nyc.yearup.org>
2018-11-07 15:25:35 -0500

    finished

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 
-document.getElementById("color").onkeydown = function() {myFunction()};
+document.getElementById("color").onKeyDown = function() {myFunction()};
 
 function myFunction() {
     document.getElementById("color").style.backgroundColor = "darkseagreen";
 }
+
+ function startanimation() {
+   let key =  document.querySelector("#slide")
+   if (900 <= window.scrollY && 1400 >= window.scrollY){
+     key.className = "animated slider";
+   } else {
+     key.className = "";
+   }
+
+ }
+ window.addEventListener("scroll", startanimation);
